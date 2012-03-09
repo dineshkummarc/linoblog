37305cefb7066f26749c233744cb4f1297cc27fe
Limon Lin <limon7@gmail.com>
2012-03-09 20:36:44 +0800

    update demo database

--- linoblog.sql
+++ linoblog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2012 年 03 月 08 日 14:13
+-- 生成日期: 2012 年 03 月 09 日 13:36
 -- 服务器版本: 5.5.16
 -- PHP 版本: 5.3.8
 
@@ -31,14 +31,17 @@ CREATE TABLE IF NOT EXISTS `category` (
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- 转存表中的数据 `category`
 --
 
 INSERT INTO `category` (`id`, `name`) VALUES
-(1, 'untitled');
+(1, 'untitled'),
+(2, 'soloX'),
+(6, 'SONG'),
+(4, 'RT');
 
 -- --------------------------------------------------------
 
@@ -53,38 +56,34 @@ CREATE TABLE IF NOT EXISTS `post` (
   `pubdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `category_id` int(10) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=35 ;
 
 --
 -- 转存表中的数据 `post`
 --
 
 INSERT INTO `post` (`id`, `title`, `content`, `pubdate`, `category_id`) VALUES
-(1, 'test', 'long long ago...', '2012-04-04 16:00:00', 1),
+(1, 'test', 'long long ago...33CCQQ', '2012-04-04 16:00:00', 2),
 (2, 'test', 'long long ago...', '2012-03-04 16:00:00', 1),
 (4, 'test', 'long long ago...', '2012-03-04 16:00:00', 1),
 (5, 'test', 'long long ago...', '2012-03-04 16:00:00', 1),
-(6, 'test', 'long long ago...', '2012-03-06 16:00:00', 1),
+(6, 'test', 'long long ago...', '2012-03-06 16:00:00', 4),
 (7, 'test', 'long long ago...xx', '2012-03-06 16:00:00', 1),
 (8, 'test', 'long long ago...55-=\r\n', '2012-03-06 16:00:00', 1),
-(9, 'test', 'long long ago...', '2012-03-06 16:00:00', 1),
-(10, 'test', 'long long ago...', '2012-03-06 16:00:00', 1),
 (11, 'test', 'long long ago...', '2012-03-06 16:00:00', 1),
 (12, 'testxge', 'long long ago...gg', '2012-03-06 16:00:00', 1),
 (13, 'test', 'long long ago...00', '2012-03-06 16:00:00', 1),
-(14, 'test', 'long long ago...', '0000-00-00 00:00:00', 1),
-(15, 'test', 'long long ago...xxx', '0000-00-00 00:00:00', 1),
 (16, 'ehw', 'eweew', '2012-03-08 08:37:33', 1),
-(17, 'ehw', 'eweew', '2012-03-08 08:38:15', 1),
+(17, 'ehw', 'eweew', '2012-03-08 08:38:15', 4),
 (18, 'eee', 'cgwgeg''''', '2012-03-08 08:38:25', 1),
-(19, 'eee', 'cgwgeg', '2012-03-08 08:41:33', 1),
-(20, 'rehc', 'ccc', '2012-03-08 08:42:54', 1),
-(21, 'rehc', 'ccc', '2012-03-08 08:43:33', 1),
-(22, 'rehc', 'ccc', '2012-03-08 08:43:36', 1),
-(23, 'rehc', 'ccc', '2012-03-08 08:43:38', 1),
-(24, 'OMG', 'YES YES', '2012-03-08 10:39:25', 1),
-(25, 'newwen', 'hoho', '2012-03-08 11:39:35', 1),
-(26, 'newwen', 'hoho', '2012-03-08 11:39:57', 1);
+(19, 'eee', 'cgwgegQQQQ', '2012-03-08 08:41:33', 1),
+(20, 'GB34', 'cccGB', '2012-03-08 08:42:54', 2),
+(32, 'dqweewrwrwq33', 'Geeee', '2012-03-09 12:21:11', 1),
+(34, '566e', 'Gee eee', '2012-03-09 12:21:29', 1),
+(30, 'dqweetewq', 'Geeee', '2012-03-09 12:21:01', 1),
+(29, 'dqwweg', 'Gehehwe', '2012-03-09 12:20:55', 1),
+(33, '33ggwe', 'Gee eee', '2012-03-09 12:21:24', 1),
+(31, 'dqweewrwrwq', 'Geeee', '2012-03-09 12:21:06', 1);
 
 -- --------------------------------------------------------
 
